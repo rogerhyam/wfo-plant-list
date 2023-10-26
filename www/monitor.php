176b98a8feab2d904f348b87f8ada18c6744b64f
Roger Hyam <rhyam@rbge.org.uk>
2023-10-26 16:42:18 +0100

    added hidden monitoring

---
+++ www/monitor.php
@@ -0,0 +1,91 @@
+<?php
+require_once('header.php');
+?>
+<h2>Performance Monitors</h2>
+<p>This page shows the performance banners for the monitors that are run by the Pingdom service. Roger gets email
+    notifications of outages. You could get them to if you ask him to set you up!</p>
+
+<h3>Main Portal</h3>
+<p>
+    <a
+        href="https://list.worldfloraonline.org/rhakhis/ui/index.html">https://list.worldfloraonline.org/rhakhis/ui/index.html</a>
+<p>
+<p>
+    <a href="https://www.pingdom.com"><img src="https://share.pingdom.com/banners/5c6b1b5e"
+            alt="Uptime Report for WFO Portal: Last 30 days" title="Uptime Report for WFO Portal: Last 30 days"
+            width="300" height="165" /></a>
+    <a href="https://www.pingdom.com"><img src="https://share.pingdom.com/banners/e26c1251"
+            alt="Uptime Report for WFO Portal: Last 30 days" title="Uptime Report for WFO Portal: Last 30 days"
+            width="300" height="165" /></a>
+</p>
+<h3>About Pages</h3>
+<p>
+    <a href="https://about.worldfloraonline.org/">https://about.worldfloraonline.org/</a>
+<p>
+<p>
+    <a href="https://www.pingdom.com"><img src="https://share.pingdom.com/banners/caf03d16"
+            alt="Uptime Report for WFO About Pages: Last 30 days"
+            title="Uptime Report for WFO About Pages: Last 30 days" width="300" height="165" /></a>
+    <a href="https://www.pingdom.com"><img src="https://share.pingdom.com/banners/93eb2bdd"
+            alt="Uptime Report for WFO About Pages: Last 30 days"
+            title="Uptime Report for WFO About Pages: Last 30 days" width="300" height="165" /></a>
+
+</p>
+
+<h3>WFO Plant List</h3>
+<p>
+    <a href="https://wfoplantlist.org/plant-list/">https://wfoplantlist.org/plant-list/</a>
+<p>
+<p>
+    <a href="https://www.pingdom.com"><img src="https://share.pingdom.com/banners/b5869bd1"
+            alt="Uptime Report for PlantList Front End: Last 30 days"
+            title="Uptime Report for PlantList Front End: Last 30 days" width="300" height="165" /></a>
+    <a href="https://www.pingdom.com"><img src="https://share.pingdom.com/banners/6027e7c0"
+            alt="Uptime Report for PlantList Front End: Last 30 days"
+            title="Uptime Report for PlantList Front End: Last 30 days" width="300" height="165" /></a>
+</p>
+<h3>WFO Plant List API</h3>
+<p>
+    <a href="https://list.worldfloraonline.org/">https://list.worldfloraonline.org/</a>
+<p>
+<p>
+    <a href="https://www.pingdom.com"><img src="https://share.pingdom.com/banners/22d35f60"
+            alt="Uptime Report for Worldfloraonline List: Last 30 days"
+            title="Uptime Report for Worldfloraonline List: Last 30 days" width="300" height="165" /></a>
+    <a href="https://www.pingdom.com"><img src="https://share.pingdom.com/banners/176dc6ef"
+            alt="Uptime Report for Worldfloraonline List: Last 30 days"
+            title="Uptime Report for Worldfloraonline List: Last 30 days" width="300" height="165" /></a>
+
+</p>
+
+<h3>Rhakhis</h3>
+<p>
+    <a
+        href="https://list.worldfloraonline.org/rhakhis/ui/index.html">https://list.worldfloraonline.org/rhakhis/ui/index.html</a>
+<p>
+<p>
+    <a href="https://www.pingdom.com"><img src="https://share.pingdom.com/banners/cb1f5d11"
+            alt="Uptime Report for WFO Rhakhis: Last 30 days" title="Uptime Report for WFO Rhakhis: Last 30 days"
+            width="300" height="165" /></a>
+    <a href="https://www.pingdom.com"><img src="https://share.pingdom.com/banners/c3515ddf"
+            alt="Uptime Report for WFO Rhakhis: Last 30 days" title="Uptime Report for WFO Rhakhis: Last 30 days"
+            width="300" height="165" /></a>
+</p>
+
+<h3>Staging Server</h3>
+<p>
+    <a href="https://rhakhis.rbge.info/">https://rhakhis.rbge.info/</a>
+<p>
+<p>
+    <a href="https://www.pingdom.com"><img src="https://share.pingdom.com/banners/34c0dd08"
+            alt="Uptime Report for WFO Staging API: Last 30 days"
+            title="Uptime Report for WFO Staging API: Last 30 days" width="300" height="165" /></a>
+    <a href="https://www.pingdom.com"><img src="https://share.pingdom.com/banners/a90a969d"
+            alt="Uptime Report for WFO Staging API: Last 30 days"
+            title="Uptime Report for WFO Staging API: Last 30 days" width="300" height="165" /></a>
+</p>
+
+
+<?php
+require_once('footer.php');
+?>
