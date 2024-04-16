a526a062ca9a10cc20dbef800c02239ad3febc61
Roger Hyam <r.hyam@rbge.org.uk>
2024-04-16 12:38:06 +0100

    Bootstrapping

--- www/header.php
+++ www/header.php
@@ -2,7 +2,14 @@
 <html>
 
 <head>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>WFO Plant List: Name Matching</title>
+
+
+
     <style>
     body {
         font-family: sans-serif;
@@ -111,12 +118,71 @@
 
 <body>
 
-    <?php
-        if($plant_list_system_message){
-        echo "<p style=\"background-color: black; color:white; padding: 0.3em; border: solid 1px gray; margin: 0px;\"><strong>&nbsp;⚠️&nbsp;System Message:&nbsp;</strong>$plant_list_system_message</p>";
-        echo "<hr/>";
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">WFO Plant List API</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'sw_index.php' ? 'active': '';  ?>"
+                            aria-current="page" href="sw_index.php">Stable URIs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'gql_index.php' ? 'active': '';  ?> "
+                            href="gql_index.php">GraphQL API</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'matching.php' ? 'active': '';  ?> "
+                            href="matching.php">Matching Tool</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'matching_rest.php' ? 'active': '';  ?> "
+                            href="matching_rest.php">Matching API</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'reconcile_index.php' ? 'active': '';  ?> "
+                            href="reconcile_index.php">Reconciliation API</a>
+                    </li>
+
+
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'references.php' ? 'active': '';  ?> "
+                            href="references.php">Refs Tool</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'stats.php' ? 'active': '';  ?> "
+                            href="stats.php">Stats</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'browser.php' ? 'active': '';  ?> "
+                            href="browser.php">Browser</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main class="container">
+        <div class="bg-light p-5 rounded">
+            <?php
+    if($plant_list_system_message){
+        echo "<p><div class=\"alert alert-danger\" role=\"alert\"><strong>&nbsp;System Message:&nbsp;</strong>$plant_list_system_message</div></p>";
+//        echo "<p style=\"background-color: black; color:white; padding: 0.3em; border: solid 1px gray; margin: 0px;\"><strong>&nbsp;⚠️&nbsp;System Message:&nbsp;</strong>$plant_list_system_message</p>";
+  //      echo "<hr/>";
     }
-    ?>
+?>
+            <!--
     <div id="navbar">
         <strong>WFO Plant List: </strong>
         <a href="index.php">Home</a>
@@ -137,6 +203,6 @@
         |
         <a href="browser.php">Browser</a>
     </div>
+-->
 
-
-    <!-- end header.php -->
+            <!-- end header.php -->
